01667a20e9efc4dfd824ba5c7c31242bfc675fd1
nphuoctho <nguyenphuoctho0310@gmail.com>
2024-06-11 10:10:13 +0700

    feat: init setup

---
+++ src/index.spec.ts
@@ -0,0 +1,18 @@
+import { describe } from "node:test";
+import supertest from "supertest";
+import { server } from "./index";
+
+describe("Server", () => {
+  const request = supertest.agent(server);
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it("should get", async () => {
+    const res = await request.get("/");
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({ data: "It Works" });
+  });
+});
